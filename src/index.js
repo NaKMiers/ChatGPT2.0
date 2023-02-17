101b2c4758a5b1bb8a8edd0011ab3fad41d4a072
Pi Pi <diwas118151@gmail.com>
2023-02-17 19:20:12 +0700

    re refactor code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { Configuration, OpenAIApi } = require('openai')
+const routes = require('./routes')
 
 // express instance
 const app = express()
@@ -20,31 +21,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 // routes
-app.get('/', (req, res) => {
-   res.send('<h1>This is server of ChatGPT2.0</h1>')
-})
-
-app.post('/', async (req, res) => {
-   console.log('post message')
-   const { message } = req.body
-   console.log('message: ', message)
-
-   try {
-      const response = await openai.createCompletion({
-         model: 'text-davinci-003',
-         prompt: `${message}`,
-         max_tokens: 100,
-         temperature: 0.5,
-      })
-
-      console.log('response: ', response.data)
-
-      res.json({ message: response.data.choices[0].text })
-   } catch (err) {
-      console.log(err)
-      res.send(err).status(400)
-   }
-})
+routes(app, openai)
 
 // app listening
 app.listen(process.env.PORT, () => console.log('Server listening on port: ' + process.env.PORT))
